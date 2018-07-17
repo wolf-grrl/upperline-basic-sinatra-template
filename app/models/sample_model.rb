b69081dc0342e964bc4f046168cd9dfc4fe97f46
wolf-grrl <bluwolfess@gmail.com>
2018-07-17 21:08:13 +0000

    bleh

--- app/models/sample_model.rb
+++ app/models/sample_model.rb
@@ -1,3 +1,17 @@
-class SampleModel
-  attr_accessor :name, :age
+def kwiz_return(response1, response2)
+  if response1 == "array" && resonse2 == "pancakes"
+    puts "WEIRD!YOU are an array who likes pancakes!"
+  elsif response1 == "array" && response2 == "waffles"
+        "OOOOOOOOOOOOOOOOO SPIIICYYYY!"
+    elsif response1 == "hash" && response2 == "pancakes"
+        "you are insufferable."
+    elsif response1 == "hash" && response2 == "waffles"
+        "hash and waffles? more like hashBROWNS and waffles"
+    else
+
+
+
+
+
+
 end
